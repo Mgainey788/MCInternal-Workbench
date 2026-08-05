fba3cf7be87b41380bd5e0e7e162c1992cb246f1
Mgainey788 <164232699+Mgainey788@users.noreply.github.com>
2026-08-05 15:40:00 +0000

    Remove conflicting startup.sh and duplicate .python-version

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/site/wwwroot
-/home/site/wwwroot/antenv/bin/streamlit run streamlit_app.py \
-  --server.address 0.0.0.0 \
-  --server.port $WEBSITES_PORT \
-  --server.headless true
